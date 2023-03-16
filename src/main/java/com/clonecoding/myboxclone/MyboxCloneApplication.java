9340940c623ac15929430daaeebd17419e9d7156
Jaehyeon <112854083+jaehyeonkim2358@users.noreply.github.com>
2023-03-16 14:40:32 +0900

    프로젝트 생성

---
+++ src/main/java/com/clonecoding/myboxclone/MyboxCloneApplication.java
@@ -0,0 +1,13 @@
+package com.clonecoding.myboxclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboxCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboxCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clonecoding/myboxclone/MyboxCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clonecoding.myboxclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboxCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
